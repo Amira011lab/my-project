88bfb04b033da22906a87eeb3dc5e9376cac3dc8
Amira011lab <your_amiramuze8@gmail.com>
2026-02-19 11:17:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Greeting from './components/Greeting'
+import { Header } from './components/header'
+function App() {
+
+  return (
+    <>
+      {/*<Greeting name = "Amira" surname="Muse"/>*/}
+      <Header />
+
+
+    </>
+  )
+}
+
+export default App
